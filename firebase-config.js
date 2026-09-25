e35c6a33953bbe51d4bdc3b0753c0b2fb8bbd9ee
nihongoclass <bonny19751115@gmail.com>
2026-09-25 11:52:44 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase 設定檔
+// 建立 Firebase 專案後，把 firebaseConfig 貼在下面。
+// databaseURL 必須存在。
+window.FIREBASE_CONFIG = {
+  apiKey: "請貼上你的 apiKey",
+  authDomain: "請貼上你的 authDomain",
+  databaseURL: "請貼上你的 databaseURL",
+  projectId: "請貼上你的 projectId",
+  storageBucket: "請貼上你的 storageBucket",
+  messagingSenderId: "請貼上你的 messagingSenderId",
+  appId: "請貼上你的 appId"
+};
