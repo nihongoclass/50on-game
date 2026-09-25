b2a43c43f2530c79ad7d1ceed024e39683b3b2ba
nihongoclass <bonny19751115@gmail.com>
2026-09-25 12:19:31 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,9 @@
-// Firebase 設定檔
-// 建立 Firebase 專案後，把 firebaseConfig 貼在下面。
-// databaseURL 必須存在。
 window.FIREBASE_CONFIG = {
-  apiKey: "請貼上你的 apiKey",
-  authDomain: "請貼上你的 authDomain",
-  databaseURL: "請貼上你的 databaseURL",
-  projectId: "請貼上你的 projectId",
-  storageBucket: "請貼上你的 storageBucket",
-  messagingSenderId: "請貼上你的 messagingSenderId",
-  appId: "請貼上你的 appId"
+  apiKey: "AIzaSyCtCiKnd6nsveASHTAj_fyjcWFT3B3Qj2A",
+  authDomain: "on-game-235ff.firebaseapp.com",
+  databaseURL: "https://on-game-235ff-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "on-game-235ff",
+  storageBucket: "on-game-235ff.firebasestorage.app",
+  messagingSenderId: "229867781247",
+  appId: "1:229867781247:web:784f629d665a96610dd34a"
 };
